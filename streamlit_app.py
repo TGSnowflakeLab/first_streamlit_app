aa6ce44f0c8d3ad6bd6fcac0d3a9e63c9bb8fef8
TGSnowflakeLab <105294930+TGSnowflakeLab@users.noreply.github.com>
2022-05-13 11:41:53 -0500

    Update streamlit_app.py
    Replaced fruityvice JSON response on screen with it normalized into a table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,7 @@ streamlit.dataframe(fruits_to_show);
 # display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!');
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
-streamlit.text(fruityvice_response.json());
+
+# normalizes json and outputs it to the screen as a table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+streamlit.dataframe(fruityvice_normalized);
